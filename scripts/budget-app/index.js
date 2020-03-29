5b171df937f43ac012f5f822625af4f1b6cd15c7
Shubham Kiran Latkar <43436727+shubhamlatkar@users.noreply.github.com>
2020-03-29 22:43:48 +0530

    budget-app
    update delete in process

--- scripts/budget-app/index.js
+++ scripts/budget-app/index.js
@@ -10,6 +10,19 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  var deleteItem = function(type, id) {
+    data.allItems[type].splice(
+      data.allItems[type].findIndex(element => element.id === id),
+      1
+    );
+  };
+
+  var updateValues = function(type, innerType, id, val) {
+    data.allItems[type][
+      data.allItems[type].findIndex(element => element.id === id)
+    ][innerType] = val;
+  };
+
   var calculateTotal = function(type) {
     let sum = 0;
     data.allItems[type].forEach(inc => (sum += inc.value));
@@ -48,10 +61,10 @@ var budgetController = (function() {
 
       data.allItems[type].push(newItem);
     },
-    calculateTotal: function(type) {
-      calculateTotal(type);
-    },
-    data: data
+    calculateTotal: calculateTotal,
+    data: data,
+    deleteItem: deleteItem,
+    updateValues: updateValues
   };
 })();
 
@@ -70,39 +83,52 @@ var UIController = (function(budgetController) {
     perLabel: "main-expenses-per"
   };
 
+  var clearTable = function() {
+    document
+      .getElementById(DOMStrings.incomeTable)
+      .tBodies.namedItem(DOMStrings.incomeBody).innerHTML = "";
+    document
+      .getElementById(DOMStrings.expenseTable)
+      .tBodies.namedItem(DOMStrings.expenseBody).innerHTML = "";
+  };
+
+  var updateTable = function() {
+    document
+      .getElementById(DOMStrings.incomeTable)
+      .tBodies.namedItem(
+        DOMStrings.incomeBody
+      ).innerHTML = budgetController.data.allItems.income
+      .map(
+        inc => `<tr id=${inc.id}>
+          <td id="income-desc-${inc.id}" class="desc-edit">${
+          inc.description
+        }</td>
+          <td id="income-val-${inc.id}" class="val-edit">${inc.value}</td>
+          <td><i id="income-del-${inc.id}" class="fas fa-trash-alt"></i></td>
+        </tr>`
+      )
+      .toString()
+      .replace(/,/g, "");
+    document
+      .getElementById(DOMStrings.expenseTable)
+      .tBodies.namedItem(
+        DOMStrings.expenseBody
+      ).innerHTML = budgetController.data.allItems.expense
+      .map(
+        exp => `<tr id=${exp.id}>
+          <td>${exp.description}</td>
+          <td>${exp.value}</td>
+          <td><i id="expense-del-${exp.id}" class="fas fa-trash-alt"></i></td>
+        </tr>`
+      )
+      .toString()
+      .replace(/,/g, "");
+  };
+
   return {
     DOMStrings: DOMStrings,
-    updateTable: function() {
-      document
-        .getElementById(DOMStrings.incomeTable)
-        .tBodies.namedItem(
-          DOMStrings.incomeBody
-        ).innerHTML = budgetController.data.allItems.income
-        .map(
-          inc => `<tr>
-            <td>${inc.description}</td>
-            <td>${inc.value}</td>
-            <td>${inc.id}</td>
-          </tr>`
-        )
-        .toString()
-        .replace(/,/g, "");
-      document
-        .getElementById(DOMStrings.expenseTable)
-        .tBodies.namedItem(
-          DOMStrings.expenseBody
-        ).innerHTML = budgetController.data.allItems.expense
-        .map(
-          exp => `<tr>
-            <td>${exp.description}</td>
-            <td>${exp.value}</td>
-            <td>${exp.id}</td>
-            <td>${exp.id}</td>
-          </tr>`
-        )
-        .toString()
-        .replace(/,/g, "");
-    }
+    updateTable: updateTable,
+    clearTable: clearTable
   };
 })(budgetController);
 
@@ -121,6 +147,51 @@ var controller = (function(budgetController, UIController) {
           parseFloat(e.target[2].value)
         );
       });
+    document
+      .querySelector("#" + DOMStrings.incomeTable)
+      .addEventListener("click", updateDeleteHandler);
+    document
+      .querySelector("#" + DOMStrings.incomeTable)
+      .addEventListener("focusout", updateTable);
+    document
+      .querySelector("#" + DOMStrings.expenseTable)
+      .addEventListener("click", updateDeleteHandler);
+  };
+
+  var updateTable = function(event) {
+    if (event.target.tagName === "INPUT") {
+      let type = event.target.id.toString().split("-")[1];
+      let innerType = event.target.id.toString().split("-")[2];
+      let id = parseInt(event.target.id.toString().split("-")[3]);
+      let value = event.target.value;
+      budgetController.updateValues(type, innerType, id, value);
+      updateBudget(type);
+      document.getElementById(type + "-" + innerType + "-" + id).innerHTML =
+        event.target.value;
+    }
+  };
+
+  var editHandler = function(event) {
+    let id = event.target.id;
+    let value = event.target.innerHTML;
+    document.querySelector(
+      "#" + id
+    ).innerHTML = `<input type="text" value=${value} id="input-${id}" class="edit-input"/>`;
+  };
+
+  var updateDeleteHandler = function(event) {
+    let type = event.target.id.toString().split("-")[0];
+    let innerType = event.target.id.toString().split("-")[1];
+    let id = parseInt(event.target.id.toString().split("-")[2]);
+
+    if (innerType === "del") deleteItemHandler(type, id);
+    else if (innerType === "desc" || innerType === "val") editHandler(event);
+  };
+
+  var deleteItemHandler = function(type, id) {
+    budgetController.deleteItem(type, id);
+    UIController.updateTable();
+    updateBudget(type);
   };
 
   var updateBudget = function(type) {
